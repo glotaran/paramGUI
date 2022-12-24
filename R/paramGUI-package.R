590575d5a37a4cd03479d6152551d62bca2a93ee
Joris Snellenburg <jsnel@users.noreply.github.com>
2022-12-24 01:18:51 +0100

    Holiday maintanance for re-release on CRAN (#25)
    Maintenance release on par with 2.1.4 intended for re-release on CRAN.
    
    Changes:
    * 🚇Update R GitHub actions workflow - Bring it in sync with the TIMP workflow
    * 🚇Add R linter action
    * Add spelling WORDLIST for spell_check
    * Update doi link
    * Reformat files according to styler
    * 📚 Fix spelling errors and address issues in docs
    * 🧹More tidying up of formatting - Also ran `usethis::use_tidy_description()`
    * Update version
    * More formatting
    * Update gitignore - Ran `inteRgrate::check_gitignore()`

--- R/paramGUI-package.R
+++ R/paramGUI-package.R
@@ -1,8 +1,9 @@
 #' paramGUI
 #'
 #' @name paramGUI
+#' @description Allows specification and fitting of some parameter estimation
+#' examples inspired by time-resolved spectroscopy via a Shiny GUI.
 #' @docType package
 #' @import shiny shinydashboard
 #' @importFrom TIMP initModel fitModel
 NULL
-
